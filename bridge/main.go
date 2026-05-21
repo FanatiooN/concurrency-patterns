c1bd2f2d12a0ddebac062e289ddf91c98b8614ba
fanatioon <nazirov.f.f@gmail.com>
2026-05-21 14:46:35 +0300

    feat: add bridge

--- bridge/main.go
+++ bridge/main.go
@@ -1,1 +1,63 @@
 package main
+
+import "fmt"
+
+type Formatter interface {
+	Format(level, message string) string
+}
+
+type JSONFormatter struct{}
+
+func (f JSONFormatter) Format(level, message string) string {
+	text := fmt.Sprintf("{\"level\":\"%v\", \"message\":\"%v\"}", level, message)
+	return text
+}
+
+type PlainTextFormatter struct{}
+
+func (f PlainTextFormatter) Format(level, message string) string {
+	text := fmt.Sprintf("[%v]: %v", level, message)
+	return text
+
+}
+
+type Logger struct {
+	formatter Formatter
+}
+
+type InfoLogger struct {
+	Logger
+}
+
+func (l InfoLogger) Log(message string) {
+	res := l.formatter.Format("INFO", message)
+	fmt.Println(res)
+}
+
+func NewInfoLogger(f Formatter) *InfoLogger {
+	return &InfoLogger{Logger{formatter: f}}
+}
+
+type ErrorLogger struct {
+	Logger
+}
+
+func (l ErrorLogger) Log(message string) {
+	res := l.formatter.Format("ERROR", message)
+	fmt.Println(res)
+}
+
+func NewErrorLogger(f Formatter) *ErrorLogger {
+	return &ErrorLogger{Logger{formatter: f}}
+}
+func main() {
+	infoLogger := NewInfoLogger(JSONFormatter{})
+	errLogger := NewErrorLogger(PlainTextFormatter{})
+
+	infoLogger.Log("meow1")
+	infoLogger.Log("meow2")
+
+	errLogger.Log("meow3")
+	errLogger.formatter = JSONFormatter{}
+	errLogger.Log("meow4")
+}
